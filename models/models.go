0e5727c486f537e6d1fb80acb529546ead7c5ca3
Deepanshu Mishra <d4deepanshu723@gmail.com>
2026-06-18 08:29:10 +0530

    basic models defination

--- models/models.go
+++ models/models.go
@@ -1,1 +1,24 @@
-package models;
+package models
+
+type Status string
+
+const (
+	QUEUED   Status = "queued"
+	RUNNING  Status = "running"
+	FINISHED Status = "finished"
+	FAILED   Status = "failed"
+)
+
+type User struct {
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Job struct {
+	JobID     string         `json:"jobId"`
+	JobName   string         `json:"job_name"`
+	JobStatus Status         `json:"status"`
+	Payload   map[string]any `json:"payload"`
+}
